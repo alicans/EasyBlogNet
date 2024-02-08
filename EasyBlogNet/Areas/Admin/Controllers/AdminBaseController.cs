ea71f260b920e3e32368cb3a8c2dd9f0da8a3818
Alican Dökmen <alican@outlook.com>
2024-02-08 12:40:04 +0300

    admin login check, dashboard draft

---
+++ EasyBlogNet/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBlogNet.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    [Area("Admin")]
+    public abstract class AdminBaseController : Controller
+    {
+        
+    }
+}
---
+++ EasyBlogNet/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBlogNet.Areas.Admin.Controllers
+{
+    
+    public class DashboardController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
