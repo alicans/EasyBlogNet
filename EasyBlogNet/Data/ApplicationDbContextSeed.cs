3f2b79833b324328257cdda90df7c8f5c52f2ebe
Alican Dökmen <alican@outlook.com>
2024-02-08 12:12:36 +0300

    admin seeding..

---
+++ EasyBlogNet/Data/ApplicationDbContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyBlogNet.Data
+{
+    public static class ApplicationDbContextSeed
+    {
+        public async static Task SeedAsync(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            if (!await roleManager.RoleExistsAsync("Administrator"))
+                await roleManager.CreateAsync(new IdentityRole("Administrator"));
+
+            if (!await userManager.Users.AnyAsync(u => u.UserName == "admin@example.com"))
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = "admin@example.com",
+                    Email = "admin@example.com",
+                    EmailConfirmed = true
+                };
+
+                await userManager.CreateAsync(user, "P@ssword1");
+                await userManager.AddToRoleAsync(user, "Administrator");
+            }
+
+
+
+
+        }
+    }
+}
--- EasyBlogNet/Program.cs
+++ EasyBlogNet/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
@@ -39,5 +40,12 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    await ApplicationDbContextSeed.SeedAsync(roleManager, userManager);
+}
 
 app.Run();
